e4aa9c04a79023b4bee0f24c3d2b47e04bffcc99
Casperdroid5 <caspertak1@gmail.com>
2024-02-26 14:13:08 +0100

    code optimalisation

--- test_sh1106.py
+++ test_sh1106.py
@@ -1,9 +1,9 @@
 from machine import Pin, I2C
 import sh1106
 import time 
+
 # Define the pins for I2C communication
-scl_pin = Pin(1)  # SCL pin
-sda_pin = Pin(0)  # SDA pin
+scl_pin, sda_pin = Pin(1), Pin(0)
 
 # Initialize I2C bus
 i2c = I2C(0, scl=scl_pin, sda=sda_pin, freq=400000)
@@ -11,20 +11,15 @@ i2c = I2C(0, scl=scl_pin, sda=sda_pin, freq=400000)
 # Create SH1106 display object
 display = sh1106.SH1106_I2C(128, 64, i2c, Pin(16), 0x3c, 180)
 
-# Turn off sleep mode
+# Turn off sleep mode, clear the display, and display text
 display.sleep(False)
-
-# Clear the display
 display.fill(0)
-
-# Display text
 display.text('Testing 2', 0, 0, 1)
 
-# Update the display
+# Update the display and pause for 3 seconds
 display.show()
+time.sleep(3)
 
-time.sleep (3)
-
+# Clear the display again and update it
 display.fill(0)
-# Update the display
 display.show()
