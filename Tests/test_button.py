df032d0f34ec21475787edfe2efba00542b366db
Casperdroid5 <caspertak1@gmail.com>
2024-02-26 15:03:03 +0100

    cleanup and minor code improvements

--- Tests/test_button.py
+++ Tests/test_button.py
@@ -11,13 +11,13 @@ if __name__ == "__main__":
     def notpressed_callback():
         print("Button not pressed")
     # Create a Button instance with pin number 5 and the pressed_callback as the callback function
-    button = Button(pin_number=5, callback=pressed_callback)
+    button1 = Button(pin_number=5, callback=pressed_callback)
 
     # Continuously check the button state
 import time
 
 while True:  # You need a colon after `while 1` and `while True`
-    if button.is_pressed():
+    if button1.is_pressed():
         pressed_callback()  # Call the callback function
         # Add a small delay to avoid high CPU usage
         time.sleep(0.1)  # Adjust the sleep time as needed for debounce measurement
--- Tests/test_sh1106.py
+++ Tests/test_sh1106.py
@@ -1,7 +1,7 @@
 from machine import Pin, I2C
 import sh1106
 import time 
-
+import framebuf
 # Define the pins for I2C communication
 scl_pin, sda_pin = Pin(1), Pin(0)
 
@@ -10,16 +10,20 @@ i2c = I2C(0, scl=scl_pin, sda=sda_pin, freq=400000)
 
 # Create SH1106 display object
 display = sh1106.SH1106_I2C(128, 64, i2c, Pin(16), 0x3c, 180)
+brightnesslevel = 100
 
 # Turn off sleep mode, clear the display, and display text
 display.sleep(False)
 display.fill(0)
-display.text('Testing 2', 0, 0, 1)
+display.text('Testing, testing', 0, 0, 1) # (text, x, y, color)
+display.text('1,2,3', 0,10,1)
+display.contrast(brightnesslevel) #brightness 0-255
 
 # Update the display and pause for 3 seconds
 display.show()
 time.sleep(3)
 
-# Clear the display again and update it
+# Clear the display and update it
 display.fill(0)
 display.show()
+
