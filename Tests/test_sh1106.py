4afcbb0127feaad761ac069b1e5d0db043e359a6
Casperdroid5 <caspertak1@gmail.com>
2024-02-27 16:11:34 +0100

    added missing objects to main

--- Tests/test_sh1106.py
+++ Tests/test_sh1106.py
@@ -10,14 +10,14 @@ i2c = I2C(0, scl=scl_pin, sda=sda_pin, freq=400000)
 
 # Create SH1106 display object
 display = sh1106.SH1106_I2C(128, 64, i2c, Pin(16), 0x3c, 180)
-brightnesslevel = 100
+OLEDbrightnesslevel = 100
 
 # Turn off sleep mode, clear the display, and display text
 display.sleep(False)
 display.fill(0)
 display.text('Testing, testing', 0, 0, 1) # (text, x, y, color)
 display.text('1,2,3', 0,10,1)
-display.contrast(brightnesslevel) #brightness 0-255
+display.contrast(OLEDbrightnesslevel) #brightness 0-255
 
 # Update the display and pause for 3 seconds
 display.show()
--- main.py
+++ main.py
@@ -4,12 +4,17 @@ import time
 from button import Button
 from potmeter import Potentiometer
 from rgbled import RGBLED
+from led import LED
 from sh1106 import SH1106_I2C
 
 
-ledDoor2 = RGBLED(10, 11, 12)  # Example pins for RGB LED 1
-ledScanner = RGBLED(6, 7, 8)    # Example pins for RGB LED 2
-ledDoor1 = RGBLED(2, 3, 4)       # Example pins for RGB LED 3
+
+ledDoor2Lock = RGBLED(10, 11, 12) 
+ledScanner = RGBLED(6, 7, 8)      
+ledDoor1Lock = RGBLED(2, 3, 4)     
+
+ledDoor1Motor = LED(21) 
+ledDoor2Motor = LED(22) 
 
 Button1 = Button(pin_number=5, callback=None)
 Button2 = Button(pin_number=9, callback=None)
@@ -21,6 +26,5 @@ scl_pin, sda_pin = Pin(1), Pin(0) # Define the pins for I2C communication
 OLEDi2c = I2C(0, scl=scl_pin, sda=sda_pin, freq=400000) # Initialize I2C bus
 display = SH1106_I2C(128, 64, OLEDi2c, Pin(16), 0x3c, 180) # Create SH1106 display object
 
-
-brightnesslevel = 100 #brightness 0-255
+OLEDbrightnesslevel = 100 #brightness 0-255
 
