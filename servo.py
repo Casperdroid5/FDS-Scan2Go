0e802f1c74dcebe60bf2937402f12ff22c9c79a1
Casperdroid5 <caspertak1@gmail.com>
2024-03-05 21:42:48 +0100

    fixed servo class

--- servo.py
+++ servo.py
@@ -1,9 +1,9 @@
-from pwm import pwm
+from machine import PWM
 
 class Servo:
     def __init__(self, pin_number):
-        self.pwm = pwm(pin_number)
-        self.pwm.pin.freq(50)  # Set PWM frequency to 50 Hz for servo control
+        self.pwm = PWM(pin_number)
+        self.pwm.freq(50)  # Set PWM frequency to 50 Hz for servo control
         self.min_us = 600  # Minimum pulse width in microseconds for 0 degrees
         self.max_us = 2400  # Maximum pulse width in microseconds for 180 degrees
         self.current_angle = 0  # Initialize current angle to 0
@@ -13,7 +13,7 @@ class Servo:
         pulse_width_us = self.min_us + (self.max_us - self.min_us) * angle / 180
         # Convert pulse width from microseconds to duty cycle (0-65535)
         duty_cycle = int(pulse_width_us / 20000 * 65535)  # 20000 microseconds = 20 milliseconds (period)
-        self.pwm.pin.duty_u16(duty_cycle)  # Set duty cycle to control servo position
+        self.pwm.duty_u16(duty_cycle)  # Set duty cycle to control servo position
         self.current_angle = angle  # Update current angle
 
     def get_current_angle(self):
