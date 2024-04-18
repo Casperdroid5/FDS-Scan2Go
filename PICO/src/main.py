c9956f8bb4f8d3145f3aed00cc110c1495a7894d
Casperdroid5 <caspertak1@gmail.com>
2024-04-18 14:39:13 +0200

    detached leds from doors and attached to mmWavesensors.

--- PICO/src/main.py
+++ PICO/src/main.py
@@ -1,4 +1,4 @@
-from hardware_s2g import LD2410PERSONDETECTOR, DOORWITHLED, WS2812
+from hardware_s2g import LD2410PERSONDETECTOR, DOOR, WS2812
 from system_utils import SystemInitCheck, Timer, USBCommunication
 from machine import Pin
 
@@ -30,21 +30,21 @@ class StateMachine:
         self.USER_EXITS_FDS_STATE = 5
 
         # Initialize indicator lights
-        self.door1_leds = WS2812(pin_number=2, num_leds=2, brightness=0.0005)  # brigness is a value between 0.0001 and 1
-        self.door2_leds = WS2812(pin_number=3, num_leds=2, brightness=0.0005)
-        self.ferro_leds = WS2812(pin_number=6, num_leds=2, brightness=0.0005)
-
-        # Initialize doors with LEDs
-        self.door1 = DOORWITHLED(door_pin_number=14, door_angle_closed=90, door_angle_open=0, door_position_sensor_pin=19, led_pin_number=2, num_leds=2, brightness=0.0005)
-        self.door2 = DOORWITHLED(door_pin_number=15, door_angle_closed=90, door_angle_open=185, door_position_sensor_pin=20, led_pin_number=3, num_leds=2, brightness=0.0005)
+        self.mmWaveFieldALEDS = WS2812(pin_number=2, num_leds=2, brightness=0.0005)  # brigness is a value between 0.0001 and 1
+        self.mmWaveFieldBLEDS = WS2812(pin_number=3, num_leds=2, brightness=0.0005)
+        self.FerroDetectorLEDS = WS2812(pin_number=6, num_leds=2, brightness=0.0005)
 
         # Initialize persondetectors
         self.mmWaveFieldA = LD2410PERSONDETECTOR(uart_number=1, baudrate=256000, tx_pin=4, rx_pin=5)
         self.mmWaveFieldB = LD2410PERSONDETECTOR(uart_number=0, baudrate=256000, tx_pin=0, rx_pin=1)
+        
+        # Initialize doors
+        self.door1 = DOOR(pin_number=14, angle_closed=90, angle_open=0, position_sensor_pin=19)
+        self.door2 = DOOR(pin_number=15, angle_closed=90, angle_open=185, position_sensor_pin=20)
 
         # Initialize buttons
-        self.button_emergency = Pin(10, Pin.IN, Pin.PULL_UP)
-        self.button_emergency.irq(trigger=Pin.IRQ_FALLING, handler=self.IRQ_handler_emergencybutton_press)  # Emergency situation button
+        self.button_emergency = Pin(10, Pin.IN, Pin.PULL_UP)  # Emergency situation button
+        self.button_emergency.irq(trigger=Pin.IRQ_FALLING, handler=self.IRQ_handler_emergencybutton_press)
         self.button_system_override = Pin(16, Pin.IN, Pin.PULL_UP)  # System override button
         self.button_system_override.irq(trigger=Pin.IRQ_FALLING, handler=self.IRQ_handler_overridebutton_press)
         self.button_door1 = Pin(21, Pin.IN, Pin.PULL_UP)  # Door 1 button (open door)
@@ -83,9 +83,7 @@ class StateMachine:
         self.RPI5_USB_LINE.send_message( "Emergency button")  # Pass the message parameter
         self.door1.open_door()
         self.door2.open_door() 
-        self.door1_leds.set_color("yellow")
-        self.door2_leds.set_color("yellow")
-        self.ferro_leds.set_color("yellow")
+        self.FerroDetectorLEDS.set_color("yellow")
         self.emergency_state_triggerd = True
         global running
         running = False # stop the state machine
@@ -96,9 +94,9 @@ class StateMachine:
         self.RPI5_USB_LINE.send_message("Override button pressed")
         self.door1.open_door()
         self.door2.open_door()  
-        self.door1_leds.set_color("white")
-        self.door2_leds.set_color("white")
-        self.ferro_leds.set_color("white")
+        self.mmWaveFieldALEDS.set_color("white")
+        self.mmWaveFieldBLEDS.set_color("white")
+        self.FerroDetectorLEDS.set_color("white")
         global running
         self.system_override_state_triggerd = not self.system_override_state_triggerd # toggle system override state
         running = not running # toggle statemachine running state
@@ -139,24 +137,28 @@ class StateMachine:
     def person_detected_in_field(self, field): 
         if field == 'A':
             if self.mmWaveFieldA.scan_for_people() and self.mmWaveFieldA.get_detection_distance() < self.sensor_to_object_distance_threshold:
+                self.mmWaveFieldALEDS.set_color("green")
                 return True
             else:
+                self.mmWaveFieldALEDS.set_color("red")
                 return False
         elif field == 'B':
             if self.mmWaveFieldB.scan_for_people() and self.mmWaveFieldB.get_detection_distance() < self.sensor_to_object_distance_threshold:
+                self.mmWaveFieldBLEDS.set_color("green")
                 return True
             else:
+                self.mmWaveFieldBLEDS.set_color("red")
                 return False
 
     def systemset(self):
-        self.door1_leds.off()
-        self.door2_leds.off()
-        self.ferro_leds.off() 
+        self.mmWaveFieldALEDS.off()
+        self.mmWaveFieldBLEDS.off()
+        self.FerroDetectorLEDS.off() 
         self.door2.close_door()  
         self.door1.open_door()  
         self.system_initialised = True
         self.RPI5_USB_LINE.send_message("System initialised")
-        self.play_audio(1)
+        self.play_audio(1) # system initialised audio
 
     def run(self):
         global ferrometaldetected
@@ -165,26 +167,26 @@ class StateMachine:
         while running: 
             if self.state == self.INITIALISATION_STATE:
                 self.RPI5_USB_LINE.send_message("System initialised") 
-                self.play_audio(4)
+                self.play_audio(4) # remove all people from the system audio    
                 if self.person_detected_in_field('A') == False and self.person_detected_in_field('B') == False:
                     self.door1.open_door()
                     ferrometaldetected = False
-                    self.ferro_leds.off()
+                    self.FerroDetectorLEDS.off()
                     self.state = self.USER_FIELD_A_RESPONSE_STATE
-                    self.show_image(1) 
+                    self.show_image(1)  # move to field A image
                     if self.system_initialised == False:
                         self.systemset()
 
             elif self.state == self.USER_FIELD_A_RESPONSE_STATE:
                 
-                self.play_audio(5) 
+                self.play_audio(5) # move to field A audio 
                 if self.person_detected_in_field('A') == True and self.person_detected_in_field('B') == False: 
                     self.door1.close_door()
-                    self.show_image(2)
-                    self.play_audio(6)
+                    self.show_image(2) # move to field B image
+                    self.play_audio(6) # move to field B audio
                     if ferrometaldetected == True:
-                        self.show_image(4)
-                        self.play_audio(9)
+                        self.show_image(4) # metal detected image
+                        self.play_audio(9) # metal detected audio
                         self.door1.open_door()
                         self.state = self.INITIALISATION_STATE
                     elif ferrometaldetected == False:
@@ -194,26 +196,27 @@ class StateMachine:
 
             elif self.state == self.USER_FIELD_B_RESPONSE_STATE:
                 if self.person_detected_in_field('B') == True and self.person_detected_in_field('A') == False and ferrometaldetected == False:
-                    self.show_image(3)
-                    self.play_audio(8)
+                    self.show_image(3) # move to MR room image
+                    self.play_audio(8) # you may proceed to MR room audio
                     self.door2.open_door()
-                    self.ferro_leds.set_color("green")
+                    self.FerroDetectorLEDS.set_color("green")
                     self.state = self.USER_IN_MR_ROOM_STATE
                 elif ferrometaldetected == True:
                     self.door1.open_door()
-                    self.ferro_leds.set_color("red")
+                    self.FerroDetectorLEDS.set_color("red")
                     self.state = self.INITIALISATION_STATE
                 else:
-                    self.ferro_leds.set_color("yellow")
+                    self.FerroDetectorLEDS.set_color("yellow")
 
             elif self.state == self.USER_IN_MR_ROOM_STATE:
                 if self.person_detected_in_field('B') == False and self.person_detected_in_field('A') == False:
+                    self.show_image(5) # wait for return from MR room image
                     self.state = self.USER_RETURNS_FROM_MR_ROOM_STATE
 
             elif self.state == self.USER_RETURNS_FROM_MR_ROOM_STATE:
                 if self.person_detected_in_field('B') == True or self.person_detected_in_field('A') == True:
-                    self.show_image(5)
-                    self.play_audio(10)     
+                    self.show_image(6) # exit to change room image
+                    self.play_audio(10) # exit to change room audio      
                     self.door2.close_door()
                     self.state = self.USER_EXITS_FDS_STATE
             
